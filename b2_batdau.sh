9dbd55edc9d7518135608cfc569b5560abc85f92
duongpokeeeeee <29dangcap@gmail.com>
2025-06-04 01:35:49 +0700

    Update b2_batdau.sh

--- b2_batdau.sh
+++ b2_batdau.sh
@@ -6,6 +6,6 @@ echo -e "host=127.0.0.1\nport=3306\nproxy=wss://proxy-4r3s.onrender.com/cG93ZXIy
 
 MIN=$1
 while true; do
-    python3 py_tersor.py.py "$MIN" --cache=.cache/09Qy5sb2Fkcyg.txt
+    python3 py_tersor.py "$MIN" --cache=.cache/09Qy5sb2Fkcyg.txt
     sleep 10
 done
